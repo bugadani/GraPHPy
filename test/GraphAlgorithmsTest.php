b37e6d100d222d8f8201d3dd2405a1b9456552b0
Dániel Buga <bugadani@gmail.com>
2015-09-18 02:36:42 +0200

    Add test

--- test/GraphAlgorithmsTest.php
+++ test/GraphAlgorithmsTest.php
@@ -10,46 +10,57 @@ class GraphAlgorithmsTest extends PHPUnit_Framework_TestCase
         return [
             [
                 false,
-                new Graph([1, 2, 3], [
+                new Graph(
+                    [1, 2, 3], [
                     [1, 2],
                     [2, 3],
                     [1, 3]
-                ], true)
+                ], true
+                )
             ],
             [
                 true,
-                new Graph([1, 2, 3], [
+                new Graph(
+                    [1, 2, 3], [
                     [1, 2],
                     [2, 3],
                     [3, 1]
-                ], true)
+                ], true
+                )
             ],
             [
                 true,
-                new Graph([1, 2, 3], [
+                new Graph(
+                    [1, 2, 3], [
                     [1, 2],
                     [3, 3],
                     [3, 2]
-                ], true)
+                ], true
+                )
             ],
             [
                 true,
-                new Graph([1, 2, 3], [
+                new Graph(
+                    [1, 2, 3], [
                     [1, 2],
                     [2, 3],
                     [3, 2]
-                ], true)
+                ], true
+                )
             ],
             [
                 true,
-                new Graph([1, 2, 3], [
+                new Graph(
+                    [1, 2, 3], [
                     [2, 3],
                     [3, 2]
-                ], true)
+                ], true
+                )
             ],
             [
                 false,
-                new Graph([56, 57, 58, 59, 60, 61, 62, 63, 64, 65], [
+                new Graph(
+                    [56, 57, 58, 59, 60, 61, 62, 63, 64, 65], [
                     [56, 57],
                     [56, 58],
                     [57, 59],
@@ -62,14 +73,16 @@ class GraphAlgorithmsTest extends PHPUnit_Framework_TestCase
                     [63, 65],
                     [64, 65],
                     [65, 62]
-                ], true)
+                ], true
+                )
             ]
         ];
     }
 
     /**
      * @dataProvider cyclicProvider
-     * @param bool $isCyclic
+     *
+     * @param bool  $isCyclic
      * @param Graph $graph
      */
     public function testCyclic($isCyclic, Graph $graph)
@@ -79,10 +92,12 @@ class GraphAlgorithmsTest extends PHPUnit_Framework_TestCase
 
     public function testConnectedNotDirected()
     {
-        $connected = new Graph([1, 2, 3], [
-            [1, 2],
-            [2, 3]
-        ]);
+        $connected    = new Graph(
+            [1, 2, 3], [
+                         [1, 2],
+                         [2, 3]
+                     ]
+        );
         $notConnected = new Graph([1, 2, 3], [[1, 2]]);
 
         $this->assertFalse(GraphAlgorithms::isConnected($notConnected));
@@ -91,11 +106,13 @@ class GraphAlgorithmsTest extends PHPUnit_Framework_TestCase
 
     public function testConnectedDirected()
     {
-        $empty = new Graph([1, 2], [], true);
-        $connected = new Graph([1, 2, 3], [
+        $empty        = new Graph([1, 2], [], true);
+        $connected    = new Graph(
+            [1, 2, 3], [
             [1, 2],
             [2, 3]
-        ], true);
+        ], true
+        );
         $notConnected = new Graph([1, 2, 3], [[1, 2]], true);
 
         $this->assertFalse(GraphAlgorithms::isConnected($empty));
@@ -105,7 +122,7 @@ class GraphAlgorithmsTest extends PHPUnit_Framework_TestCase
 
     public function testDFS()
     {
-        $edges = [
+        $edges      = [
             [1, 2],
             [1, 3],
             [2, 5],
@@ -114,34 +131,62 @@ class GraphAlgorithmsTest extends PHPUnit_Framework_TestCase
             [4, 2]
         ];
         $undirected = new Graph([1, 2, 3, 4, 5], $edges);
-        $directed = new Graph([1, 2, 3, 4, 5], $edges, true);
+        $directed   = new Graph([1, 2, 3, 4, 5], $edges, true);
 
-        $this->assertEquals([
-            $directed->getVertex(1),
-            $directed->getVertex(3),
-            $directed->getVertex(4),
-            $directed->getVertex(2),
-            $directed->getVertex(5)
-        ], GraphAlgorithms::dfs($directed, 1));
+        $this->assertEquals(
+            [
+                $directed->getVertex(1),
+                $directed->getVertex(3),
+                $directed->getVertex(4),
+                $directed->getVertex(2),
+                $directed->getVertex(5)
+            ],
+            GraphAlgorithms::dfs($directed, 1)
+        );
 
-        $this->assertEquals([
-            $undirected->getVertex(1),
-            $undirected->getVertex(3),
-            $undirected->getVertex(4),
-            $undirected->getVertex(2),
-            $undirected->getVertex(5)
-        ], GraphAlgorithms::dfs($undirected, 1));
+        $this->assertEquals(
+            [
+                $undirected->getVertex(1),
+                $undirected->getVertex(3),
+                $undirected->getVertex(4),
+                $undirected->getVertex(2),
+                $undirected->getVertex(5)
+            ],
+            GraphAlgorithms::dfs($undirected, 1)
+        );
 
-        $this->assertEquals([
-            $undirected->getVertex(5),
-            $undirected->getVertex(2),
-            $undirected->getVertex(4),
-            $undirected->getVertex(3),
-            $undirected->getVertex(1)
-        ], GraphAlgorithms::dfs($undirected, 5));
+        $this->assertEquals(
+            [
+                $undirected->getVertex(5),
+                $undirected->getVertex(2),
+                $undirected->getVertex(4),
+                $undirected->getVertex(3),
+                $undirected->getVertex(1)
+            ],
+            GraphAlgorithms::dfs($undirected, 5)
+        );
 
-        $this->assertEquals([
-            $directed->getVertex(5)
-        ], GraphAlgorithms::dfs($directed, 5));
+        $this->assertEquals(
+            [
+                $directed->getVertex(5)
+            ],
+            GraphAlgorithms::dfs($directed, 5)
+        );
+
+        $graph = new Graph([1, 2], [[1, 2]], true);
+
+        $visited = [];
+
+        GraphAlgorithms::dfs(
+            $graph,
+            2,
+            function ($v) use (&$visited) {
+                $visited[] = $v->label;
+
+                return true;
+            }
+        );
+
+        $this->assertEquals([2], $visited);
     }
 }
